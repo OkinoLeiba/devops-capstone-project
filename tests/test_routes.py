b90175cb99d39e86676b0048b32e52c483b6fe25
okinoleiba <okino.leiba@yahoo.com>
2026-03-13 18:06:59 -0400

    added nose task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -123,7 +123,6 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
     def test_get_account(self):
         """It should Read Account data"""
         # account = AccountFactory()
@@ -141,8 +140,6 @@ class TestAccountService(TestCase):
         # self.assertEqual(resp.status_code, status.HTTP_200_OK)
         # data = response.get_json()
         # self.assertEqual(data["name"], account.name)
-
-
         account = self._create_accounts(1)[0]
         resp = self.client.get(
             f"{BASE_URL}/{account.id}", content_type="application/json"
@@ -184,10 +181,7 @@ class TestAccountService(TestCase):
         updated_account = resp.get_json()
         self.assertEqual(updated_account["name"], "Say My Name")
 
-
-
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
